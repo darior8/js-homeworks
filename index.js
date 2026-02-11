dc6cbee14c1ebd381271646b7b956215e41110e8
Daryna <dqww888@gmail.com>
2026-02-11 11:47:17 +0200

    add cases for pow function

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-function pow(x,y) {
-    let result = 1;
+function pow(x, y) {
+    if (x === 0 && y === 0) {
+        return undefined;
+    }
 
-    for (let i=0; i < y; i++) {
+    if (y === 0) {
+        return 1;
+    }
+
+    let result = 1;
+    let power = y;
+
+    if (y < 0) {
+        power = -y;
+    }
+    for (let i = 0; i < power; i++) {
         result *= x;
     }
-return result;
+
+    if (y < 0) {
+        return 1 / result;
+    }
+
+    return result;
 }
